6febb68bef7e7373d23c6387fc50e862490a7c09
LeandroDSSF <leandro.hafmaster@gmail.com>
2018-07-14 16:30:00 -0300

    segundo commit

--- WaterBox.java
+++ WaterBox.java
@@ -4,19 +4,19 @@ import javax.swing.*;
 public class CaixaDagua {
     public static void main(String[] args){
         
-      double altura, raio,volume,soma;
-      final double PI = 3.141592;
-      final double M_cubo = 1000;
+      	double altura,raio,volume,soma;
+      	final double PI = 3.141592;
+      	final double M_cubo = 1000;
       
-      
-    altura = Integer.parseInt(JOptionPane.showInputDialog("Digite a Altura:\n"));
-    raio = Integer.parseInt(JOptionPane.showInputDialog("Digite o Raio:"));
+	      
+	    altura = Integer.parseInt(JOptionPane.showInputDialog("Digite a Altura:\n"));
+	    raio = Integer.parseInt(JOptionPane.showInputDialog("Digite o Raio:"));
+	    
+	    volume = PI * ((raio * raio) * altura);
+	    soma = volume * M_cubo;
     
-    volume = PI * ((raio * raio) * altura);
-    soma = volume * M_cubo;
+   	 JOptionPane.showMessageDialog(null,"Essa Caixa Comporta: " + soma + "m³" );
+
     
-    JOptionPane.showMessageDialog(null,"Essa Caixa Comporta: \n" + soma + "m³" );
-    
-    }
-    
+    }    
 }
